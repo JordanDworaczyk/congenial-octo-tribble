c68cfed272124041eec0b967ff413213edcff1c2
Jordan Dworaczyk <jordan.dwo@gmail.com>
2023-09-11 06:19:33 +0000

    more docstrings

--- convolution/kernel.py
+++ convolution/kernel.py
@@ -21,9 +21,16 @@ class Gaussian():
 
     @property
     def normalization_constant(self):
-        "Returns constant which normalizes gaussian kernel."
-        return 1 / np.sum(
-            gaussian(self.domain, a=1, b=self.mean, c=self.std))
+        """Returns constant which normalizes gaussian kernel.
+
+        The kernel is considered normalized when its sum is 
+        equal to one. 
+        """
+        t = self.domain
+        a = 1 
+        b = self.mean
+        c = self.std
+        return 1 / np.sum(a * np.exp(-0.5 * ((t - b) / c)**2 ))
 
     @property
     def parameter(self): 
@@ -37,7 +44,9 @@ class Gaussian():
         The set of all output values that the gaussian 
         produces over its domain.
         """
-        return gaussian(self.domain, *self.parameter)
+        t = self.domain
+        a, b, c = self.parameter
+        return a * np.exp(-0.5 * ((t - b) / c)**2 ) 
     
     @property 
     def derivative_wrt_mean(self): 
@@ -51,7 +60,7 @@ class Gaussian():
         if self.mean is not None:
             t = self.domain
             a, b, c = self.parameter
-            return a * np.exp(-0.5 * ((t - b) / c)**2 ) 
+            return a * (t - b) * np.exp(-0.5*((t - b) / c)**2) / c**2
     
     @property
     def derivative_wrt_std(self): 
@@ -60,7 +69,9 @@ class Gaussian():
         The partial derivative with respect to the standard 
         deviation. Evaulated at the parameter `self.std`. 
         """
-        return gaussian_derivative_wrt_c(self.domain, *self.parameter)
+        t = self.domain 
+        a, b, c = self.parameter
+        return a * (t - b)**2 * np.exp(-0.5*((t - b) / c)**2) / c**3 
     
     @property
     def partial_derivatives(self): 
@@ -76,43 +87,51 @@ class Gaussian():
         return [self.derivative_wrt_std]
     
 
-def gaussian(domain: npt.NDArray, a: float, b: float, c: float) -> npt.NDArray: 
-    t = domain 
-    return a * np.exp(-0.5 * ((t - b) / c)**2 ) 
-
-def gaussian_derivative_wrt_a(domain: npt.NDArray, b, c):
-    t = domain  
-    return np.exp(-0.5 * ((t - b) / c)**2) 
-
-def gaussian_derivative_wrt_b(domain: npt.NDArray, a, b, c):
-    t = domain 
-    return a * (t - b) * np.exp(-0.5*((t - b) / c)**2) / c**2 
-
-def gaussian_derivative_wrt_c(domain: npt.NDArray, a, b, c): 
-    t = domain 
-    return a * (t - b)**2 * np.exp(-0.5*((t - b) / c)**2) / c**3 
-
-class Mixture(): 
+class Mixture():
+    "Kernel that consists of a mixture of kernels."
     def __init__(self, weights, kernels): 
         self.weights = weights
         self.components = kernels
 
     @property 
     def domain(self): 
+        """Domain of the mixture. 
+
+        The union of domains for each kernel in the mixture. 
+        """
         return reduce(np.union1d, (kernel.domain for kernel in self.components))
 
     @property
     def image(self): 
+        """Image of mixture. 
+
+        The set of all output values that the 
+        kernel mixture produces over its domain.
+        """
         weights = np.array(self.weights)
         components = np.array([kernel.image for kernel in self.components])
         return weights @ components 
     
     @property 
     def derivative_wrt_weights(self): 
+        """The derivative w.r.t weights. 
+        
+        The partial derivatives of the kernel 
+        mixture with respect to the weights. The 
+        weights are assumed to be constant. Therfore 
+        the derivative is simply the corresponding 
+        components of the mixture. 
+        """
         return [kernel.image for kernel in self.components]
 
     @property 
     def derivative_wrt_components(self): 
+        """Derivative w.r.t each component in mixture.
+        
+        Returns a list of the partial derivatives of 
+        each component with respect to each of the 
+        components parameters. 
+        """
         def differentiation(w1, f, fprime, c):
             sum_fprime = np.sum(fprime)
             return w1 * (fprime - f * sum_fprime * c)
@@ -130,4 +149,9 @@ class Mixture():
 
     @property 
     def partial_derivatives(self): 
+        """Partial derivatives of kernel mixture. 
+        
+        Returns a list of the partial derivatives of the 
+        kernel mixture. 
+        """
         return [self.derivative_wrt_weights, self.derivative_wrt_components]
