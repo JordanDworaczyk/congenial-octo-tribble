a727f3d9c2ff30326272b302993d4265124b477a
Jordan Dworaczyk <jordan.dwo@gmail.com>
2023-09-12 00:08:30 -0700

    Adding convolution matrix operator

--- convolution/matrix.py
+++ convolution/matrix.py
@@ -11,3 +11,26 @@ def toeplitz(c, k):
     row[0:k+1] = c[k::-1]
     return _toeplitz(col, row)
 
+class Operator(): 
+
+    def __init__(self, kernel, structure): 
+        self.kernel = kernel 
+        self.structure = structure 
+
+    @property
+    def image(self):
+        return self.structure(self.kernel.image, self.kernel.image.size // 2)
+
+    @property
+    def partial_derivatives(self): 
+        result = list()
+        for derivatives in self.kernel.partial_derivatives: 
+            for derivative in derivatives: 
+                result.append(
+                    self.structure(
+                        derivative, 
+                        derivative.size // 2
+                    )
+                )
+        return result
+
