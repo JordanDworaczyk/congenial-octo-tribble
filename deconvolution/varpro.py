e13ebc0136db0ea7a67120aa8c0b228e8a47c4db
Jordan Dworaczyk <jordan.dwo@gmail.com>
2023-09-14 01:11:14 -0700

    changes

--- deconvolution/varpro.py
+++ deconvolution/varpro.py
@@ -13,40 +13,48 @@ def variable_projection(
     jacobian, 
     regularization_parameter, 
     regularization_matrix,
-    maximum_iterations, 
-    signal_guess=None
+    maximum_iterations
 ):
     def full_functional(y, x, d, L, alpha): 
-        K.kernel.variables = y 
+        K.kernel.variables = y # CAUTION: State state of `K.kernel.variables`
         return np.linalg.norm(K.image @ x - d)**2 + alpha**2 * np.linalg.norm(L @ x)**2
         
     K = deepcopy(guess)
-    if signal_guess is not None: 
-        KL = np.block([[K.image],  [regularization_parameter * np.identity(data.size)]])
-        D = np.block([data, np.zeros_like(data)])
-        x = pinv(KL) @ D
-        L = regularization_matrix(x)
+    L = np.identity(data.size)
     y = K.kernel.variables
     for iteration in range(maximum_iterations + 1): 
-        KL = np.block([[K.image],  [regularization_parameter * L]])
-        D = np.block([data, np.zeros_like(data)])
-
-        x = pinv(KL) @ D
-        J = jacobian(K, x)
-        residual = data - K.image @ x
-        delta_y = pinv(J) @ residual
-
-        # L = regularizatoin_matrix(x)
         old_kernel = K.kernel.image
 
-        beta = fminbound(lambda beta: full_functional(y + beta * delta_y, x, data, L, regularization_parameter), 1e-12, 1e0)
-        y = y + beta * delta_y 
-        K.kernel.variables = y
+        # Variable Projection 
+        KL = np.block([[K.image],  [regularization_parameter * L]])
+        D = np.block([data, np.zeros_like(data)])
+        x = pinv(KL) @ D
 
+        L = regularization_matrix(x) # Update Regularization Matrix
+        J = jacobian(K, x)           # Update Jacobian
+        
+        # Gauss-Newton w/ Line Search Strategy
+        residual = data - K.image @ x
+        delta_y = pinv(J) @ residual
+       
+       # Line Search Strategy 
+        beta = fminbound(
+            lambda beta: full_functional(
+                    y + beta * delta_y,
+                    x, 
+                    data, 
+                    L, 
+                    regularization_parameter
+                ), 
+            1e-12, 
+            1e0
+        )
+
+        y = y + beta * delta_y # Update Nonlinear Variable
+        K.kernel.variables = y # Update Convolution Operator
+
+        # Stopping Condition 
         mean_squared_error = np.linalg.norm(old_kernel - K.kernel.image)**2 / data.size
         if  mean_squared_error < SMALL:
             break 
-
-
-        
     return K, x, iteration
