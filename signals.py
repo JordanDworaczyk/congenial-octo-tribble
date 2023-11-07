38ab86d7522550023a2d54d8be345be798701a55
Jordan <jordan.dwo@gmail.com>
2023-11-06 22:24:13 -0800

    experiment

--- signals.py
+++ signals.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import math
 
 
 def rectangle(domain): 
@@ -13,5 +14,10 @@ def heavside(domain):
     return np.heaviside(domain, 1)
 
 # TODO
-def smooth():
-    pass 
+def smooth(domain):
+    t = np.linspace(0, 1, domain.size)
+    return  ((0.25 < t) & (t < .75))*np.sin(2*math.pi*(t - .25))**4
+
+def mix(domain): 
+    t = np.linspace(0, 1, domain.size)
+    return 0.75*((0.1 < t) & (t < 0.25)) + 0.25*((0.3 < t) & (t < 0.32)) + ((0.5 < t) & (t < 1))*np.sin(2*math.pi*t)**4
